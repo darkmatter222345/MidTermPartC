b33aa9c381713f6669eb78ca069cb68321c955ab
oseile <oseile@sheridancollege.ca>
2021-02-26 11:08:40 -0500

    Added a scanner and a input for the user to enter

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermexam_versiona_extensioncode;
+import java.util.Scanner;
+
+/**
+ *Student Name: Leroy Osei
+ * @author Leroy
+ * Student Id: 991612307
+ */
+public class TestUserprofile {
+    
+    public static void main(String[] args){
+        
+        private string 
+        Scanner input = new Scanner(System.in);
+       
+         
+         System.out.println("Please enter your name");
+         
+         String user = input.nextLine();
+         
+         
+         
+         
+         
+         
+        
+        
+        
+        
+        
+    }
+}
+    
