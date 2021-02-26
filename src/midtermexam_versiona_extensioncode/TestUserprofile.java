af623524739e031058b6813d832f1b783ec12435
oseile <oseile@sheridancollege.ca>
2021-02-26 11:17:24 -0500

    Added some strings and questions

--- src/midtermexam_versiona_extensioncode/TestUserprofile.java
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -15,13 +15,19 @@ public class TestUserprofile {
     
     public static void main(String[] args){
         
-        private string 
+        
         Scanner input = new Scanner(System.in);
        
          
          System.out.println("Please enter your name");
          
          String user = input.nextLine();
+         
+         System.out.println("Genre: Action, Romance, Horror, Educational ");
+         System.out.print("What is your favorite?");
+         
+         
+         String genre = input.nextLine(); 
          
          
          
